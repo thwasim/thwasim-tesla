3cc843bad1609139a4024521e9d30d9535d38d1e
thwasim <102340768+thwasim@users.noreply.github.com>
2022-04-05 09:49:33 +0530

    Add files via upload

---
+++ tesla.js
@@ -0,0 +1,31 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "16rem";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function toSectionOne() {
+  const element = document.getElementById("section-1");
+  element.scrollIntoView();
+}
+function toSectionTwo() {
+  const element = document.getElementById("section-2");
+  element.scrollIntoView();
+}
+function toSectionThree() {
+  const element = document.getElementById("section-3");
+  element.scrollIntoView();
+}
+function toSectionFour() {
+  const element = document.getElementById("section-4");
+  element.scrollIntoView();
+}
+function toSectionFive() {
+  const element = document.getElementById("section-5");
+  element.scrollIntoView();
+}
+function toSectionSix() {
+  const element = document.getElementById("section-6");
+  element.scrollIntoView();
+}
